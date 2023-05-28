535bbd451af8995a8de58ec4e7edb128174f75a5
MartinezFabian <FabianMartinezDev@gmail.com>
2023-05-28 20:58:31 -0300

    feat: Add functionality to add budget and remaining budget

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+// clases
+
+class Budget {
+  #userBudget;
+  #budgetRemaining;
+  #expenses;
+
+  constructor(userBudget) {
+    this.#userBudget = userBudget;
+    this.#budgetRemaining = userBudget;
+    this.#expenses = [];
+  }
+
+  get userBudget() {
+    return this.#userBudget;
+  }
+
+  set userBudget(userBudget) {
+    this.#userBudget = userBudget;
+  }
+
+  get budgetRemaining() {
+    return this.#budgetRemaining;
+  }
+
+  addExpense(expense) {
+    this.#expenses.push(Number(expense));
+  }
+}
+
+class UserInterface {
+  static insertBudget(budget) {
+    const { userBudget, budgetRemaining } = budget;
+
+    //agregar presupuesto y presupuesto restante al HTML
+    document.querySelector("#user-budget").textContent = `Presupuesto: $${userBudget}`;
+    document.querySelector("#budget-remaining").textContent = `Restante: $${budgetRemaining}`;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   main();
 });
@@ -6,6 +46,10 @@ function main() {
   // variables/constantes
   const form = document.querySelector("#form");
   const expenseList = document.querySelector(".table__body");
+
+  //objetos
+  const budget = new Budget(getBudget());
+  UserInterface.insertBudget(budget);
 
   //funciones
 
@@ -18,6 +62,4 @@ function main() {
 
     return Number(userBudget);
   }
-
-  console.log(getBudget());
 }
